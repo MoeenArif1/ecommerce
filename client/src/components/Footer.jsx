d65641b5cf87a18f16b2b9356f8769eabb142748
MoeenArif1 <bcsf20a038@pucit.edu.pk>
2023-07-02 04:00:17 +0500

    2nd commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "../css/Footer.css"
 
 export function Footer(props) {
     
 
     return (
        <div className='footer'>
-        <div className='footer-details'>
-            <div className='footer-about'>
-                <div> About</div>
-                <div>
+        <div className='footer-content'>
+            <div className='footer-col'>
+                <div className='footer-title'> About</div>
+                <div className='footer-text'>
                 Our e-commerce website specializing in tech is your one-stop destination for all things technology.
                  We offer a wide range of cutting-edge products, including smartphones, laptops, smart home devices, and gaming accessories.
                   With our user-friendly interface, secure payment options, and efficient delivery services, we aim to provide an unmatched
@@ -17,34 +18,49 @@ export function Footer(props) {
                      our platform has you covered.
                 </div>
             </div>
-            <div className='footer-contact'>
-                <div>Contact</div>
+            <div className='footer-col'>
+                <div className='footer-title'>Contact</div>
+                <div className='footer-c-item'>
+                    <img src='/icons8-location-50.png'/>
+                    <div className="footer-text"> Katchery Road، Near Anarkali Bazar, Lahore, 54000</div>
+                </div>
+                <div className='footer-c-item'>
+                    <img src='/icons8-phonelink-ring-50.png' />
+                    <div className="footer-text">03055804846</div>
+                </div>
+                <div className='footer-c-item'>
+                    <img src='icons8-email-50.png'/>
+                    <div className="footer-text">help@xyzstore.com</div>
+                </div>
 
             </div>
-            <div className='footer-categories'>
-                <div>Category</div>
-                <span className="text">Headphones</span>
-                <span className="text">Smart Watches</span>
-                <span className="text">Bluetooth Speakers</span>
-                <span className="text">Wireless Earbuds</span>
-                <span className="text">Home Theatre</span>
-                <span className="text">Projectors</span>
+            <div className='footer-col'>
+                <div className='footer-title'>Category</div>
+                <span className="footer-text">Headphones</span>
+                <span className="footer-text">Smart Watches</span>
+                <span className="footer-text">Bluetooth Speakers</span>
+                <span className="footer-text">Wireless Earbuds</span>
+                <span className="footer-text">Home Theatre</span>
+                <span className="footer-text">Projectors</span>
             </div>
-            <div className='Pages'>
-                <div>Pages</div>
-                <Link to='/Home'>Home</Link>
-                <Link to='/About'>About</Link>
-                <Link to='/Home'>Home</Link>
-                <Link to='/About'>About</Link>
-              </div>
+            <div className='footer-col pages'>
+                <div className='footer-title'>Pages</div>
+                <Link className='footer-text' to='/Home'>Home</Link>
+                <Link className='footer-text' to='/About'>About</Link>
+                <Link className='footer-text' to='/Home'>Home</Link>
+                <Link className='footer-text' to='/About'>About</Link>
+            </div>
 
         </div>
-        <div className='footer-info'>
-            <div className='footer-info-1'>
-                XYZSTORE 2023 CREATED BY PUCIT SOLUTIONS.
-            </div>
+        <div className='bottom-bar'>
+            <div className="bottom-bar-content">
+                <span className='footer-text'>
+                    XYZSTORE 2023 CREATED BY PUCIT SOLUTIONS.
+                </span>
+                <img src='/payments.png' />
+            </div> 
         </div>
-            <img src='/payments.png' />
+          
 
        </div>
     )
