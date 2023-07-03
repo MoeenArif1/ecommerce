b7e82503905a5ff7450fd2145e4d2ed9b9383dca
MoeenArif1 <bcsf20a038@pucit.edu.pk>
2023-07-04 00:02:36 +0500

    flask server added 2

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,13 @@
+import { Newsletter } from './Newsletter'
+import { Footer } from './Footer'
 function Home(props) {
     
 
     return (
         <>
             <h1>Home</h1>
+            <Newsletter/>
+            <Footer/>
         </>
     )
 }
